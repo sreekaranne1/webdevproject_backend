1c0f13fe0281766d014df7294fda15682a826160
sreekaranne1 <sreekaranne@Sreekars-MacBook-Pro.local>
2022-12-05 16:56:42 -0500

    bug fixes

--- routes/gamesRouter.js
+++ routes/gamesRouter.js
@@ -7,7 +7,7 @@ const router = express.Router();
 const { createGame } = require("../controllers/gamesController");
 
 // //Route config
-router.route("/creategame").get(auth, getactivities);
+router.route("/creategame").get(auth, createGame);
 // router.route("/search").post(auth,searchGame);
 // router.route("/logout").get(auth,logout);
 // router.route("/game/:id").get(auth, getGame)
