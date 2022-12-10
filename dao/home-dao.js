680b49e156edcb20908587c8c6c84f24ca8a6e41
sreekaranne1 <sreekaranne@Sreekars-MacBook-Pro.local>
2022-12-10 17:49:28 -0500

    sending rating

--- dao/home-dao.js
+++ dao/home-dao.js
@@ -2,14 +2,23 @@ const { User } = require("../models/models.js");
 const mongodb = require("mongodb");
 const jwt = require("jsonwebtoken");
 const dotenv = require("dotenv");
+const axios = require("axios");
 dotenv.config({ path: "../.env" });
 
 exports.getactivitiesDao = async (req, res, next) => {
   try {
-    let user = await User.findOne({ _id: req.user.id }).populate("activity");
+    let user = await User.findOne({ _id: req.user.id })
+      .populate("activity")
+      .lean();
     if (user) {
       let activities = user.activity;
       let sortedActivities = activities.sort((a, b) => b.date - a.date);
+      //api to get rating
+      for (const i in sortedActivities) {
+        sortedActivities[i]["rating"] = await apicall(
+          sortedActivities[i].gameid
+        );
+      }
       if (sortedActivities.length < 3) {
         return res.json({
           status: 200,
@@ -34,5 +43,18 @@ exports.getactivitiesDao = async (req, res, next) => {
       status: 500,
       err: err.stack,
     });
+  }
+};
+
+const apicall = async (gameid) => {
+  let response = await axios.get(
+    "https://api.rawg.io/api/games/" +
+      gameid +
+      "?key=f227150707ad40b08b9a626750b0564b"
+  );
+  if (response.status == 200) {
+    return response.data.rating;
+  } else {
+    return { error: true };
   }
 };
